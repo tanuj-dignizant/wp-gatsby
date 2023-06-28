ad802f128edf595c8722850202573f41f599ef3c
tanuj dignizant <tanujpatra.dignizant@gmail.com>
2023-06-28 19:05:23 +0530

    Fix mobile nav close button ring color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -101,7 +101,7 @@ const NavBar = () => {
                                 <div className="-mr-2">
                                     <button
                                         type="button"
-                                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[#55b64b]"
                                         onClick={() => setOpen(!open)}
                                     >
                                         <span className="sr-only">Close menu</span>
